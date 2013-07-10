d793f681da77fb4bc597004a4b8778163ee4d648
Thanos Lefteris <alefteris@gmail.com>
2013-07-10 18:45:33 +0300

    Initial html, styles and script

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,6 @@
-console.log('\'Allo \'Allo!');
+/*global L*/
+
+var map = L.mapbox.map('map', 'alefteris.map-768ho92y')
+    .setView([38.5, 24], 6);
+var markerLayer = L.mapbox.markerLayer().loadURL('//alefteris.github.io/passport-offices-greece/data/passport-offices-greece.geojson');
+map.addLayer(markerLayer);
