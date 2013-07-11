4cead71a380a05768ff2d92dc2a943b54e738e5d
Thanos Lefteris <alefteris@gmail.com>
2013-07-11 15:14:46 +0300

    New deployment to github pages

--- scripts/047bf810.main.js
+++
@@ -1,1 +0,0 @@
-var map=L.mapbox.map("map","alefteris.map-768ho92y").setView([38.5,24],6),markerLayer=L.mapbox.markerLayer().loadURL("//alefteris.github.io/passport-offices-greece/data/passport-offices-greece.geojson");map.addLayer(markerLayer);
