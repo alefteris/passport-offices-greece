57a617af83469635633f033b443e6e6de0eaf350
Thanos Lefteris <alefteris@gmail.com>
2013-07-11 15:14:01 +0300

    Provide a custom marker popup

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,27 @@
-/*global L*/
+/*global L,Handlebars*/
 
-var map = L.mapbox.map('map', 'alefteris.map-768ho92y')
-    .setView([38.5, 24], 6);
-var markerLayer = L.mapbox.markerLayer().loadURL('//alefteris.github.io/passport-offices-greece/data/passport-offices-greece.geojson');
-map.addLayer(markerLayer);
+(function () {
+    'use strict';
+
+    var map = L.mapbox.map('map', 'alefteris.map-768ho92y');
+
+    map.markerLayer.on('layeradd', function(e) {
+        var marker = e.layer,
+            props = marker.feature.properties,
+            popupContent = '';
+
+        // Create custom popup content
+        var template = Handlebars.compile($('#marker-popup-template').html());
+        popupContent = template(props);
+
+        // http://leafletjs.com/reference.html#popup
+        marker.bindPopup(popupContent,{
+            closeButton: true
+        });
+    });
+
+    map.markerLayer.loadURL('/passport-offices-greece//data/passport-offices-greece.geojson');
+
+    map.setView([38.5, 24], 6);
+
+})();
